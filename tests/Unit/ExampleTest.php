69c72445d3e141b76a517cba6dfa375e796853dc
Danielle Torres <danielletorres@w87-160-027.wireless.uvic.ca>
2023-05-23 11:10:24 -0700

    E12: Testing Email (a lot if things didn't work)

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,38 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Support\Facades\Mail;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_that_true_is_true()
+    public function createApplication()
     {
-        $this->assertTrue(true);
+        $app = require '/Users/danielletorres/Documents/lumiio/laravel-testing/bootstrap/app.php';
+        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+        return $app;
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Mail::fake();
+    }
+
+    /**
+     * @test
+     */
+    public function email_test()
+    {
+        Mail::raw('Hello World', function ($message) {
+            $message->to('foo@bar.com');
+            $message->from('bar@foo.com');
+        });
+
+        Mail::assertSent(function (\Illuminate\Mail\Mailable $mail) {
+            return $mail->hasTo('foo@bar.com') &&
+                $mail->hasFrom('bar@foo.com');
+        });
     }
 }
